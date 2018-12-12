382f97a6a999dd412acfdd14efe30ee4c9d01012
christoph <christoph.schlueter@desy.de>
2018-12-12 15:49:05 +0100

    initial commit

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from PyQt4 import QtGui
 from mainWindow import MainWindow
 from optparse import OptionParser
 
+
+def move2RightBottomCorner(win):
+    screen_geometry = QtGui.QApplication.desktop().availableGeometry()
+    screen_size = (screen_geometry.width(), screen_geometry.height())
+    win_size = (win.frameSize().width(), win.frameSize().height())
+    x = screen_size[0] - win_size[0]
+    y = screen_size[1] - win_size[1]
+    win.move(x, y)
+
 # ----------------------------------------------------------------------
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-p", "--port", dest="port",
                       help="Port number", default="16985")
-    parser.add_option("-d", "--device", dest="device",
-                      help="Device server", default='''hasylab/p22_lm4/Output''')
     parser.add_option("-s", "--sound", dest="sound",
                       help="Sound file", default='''Fire''')
     (options, _) = parser.parse_args()
@@ -18,7 +25,10 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)          # don't pass any args here!
 
     mainWindow = MainWindow(options)
+    mainWindow.move(mainWindow.width() * -3, 0)
     mainWindow.show()
+
+    move2RightBottomCorner(mainWindow)
 
     code = app.exec_()
     sys.exit(code)
--- mainWindow.py
+++ mainWindow.py
@@ -1,5 +1,6 @@
 import PyTango
 import playsound
+from notify_run import Notify
 
 import socket
 import select
@@ -9,8 +10,8 @@ import json
 from PyQt4 import QtGui, QtCore
 from mainwindow_ui import Ui_LM4_Monitor
 
-from Queue import Queue
-from Queue import Empty as emptyQueue
+# from Queue import Queue
+# from Queue import Empty as emptyQueue
 
 # ----------------------------------------------------------------------
 class MainWindow(QtGui.QMainWindow):
@@ -29,20 +30,31 @@ class MainWindow(QtGui.QMainWindow):
 
         self.setupMenu()
 
-        self.playSound = False
+        self.notifyChannel = Notify('https://notify.run/api/', 'https://notify.run/WBYSjwtglRmFnrxC')
+        self.backNotificationSent = False
+        self.lostNotificationSent = False
+
+        self.playSound = True
         self.status = False
 
+        self.statusBarLabel = QtGui.QLabel("Sound: {}".format(str(self.playSound)))
+        self.statusBar().addPermanentWidget(self.statusBarLabel)
+
         self._serverPort = options.port
-        self.device_proxy = PyTango.DeviceProxy(options.device)
+        self.LM4_proxy = PyTango.DeviceProxy('hasylab/p22_lm4/output')
+        self.V2_proxy = PyTango.DeviceProxy('hasylab/petra3_p22vil.cdi/v2')
+        self.PS2_proxy = PyTango.DeviceProxy('hasylab/petra3_p22vil.cdi/ps2')
+        self.PETRA_proxy = PyTango.DeviceProxy('petra/globals/keywords')
 
         self.sound = options.sound
 
-        self.stopServerBucket = Queue()
-        self._thread_running = False
+        # self.stopServerBucket = Queue()
+        # self._thread_running = False
+        # self._startServer()
 
         self._refreshTimer = QtCore.QTimer(self)
         self._refreshTimer.timeout.connect(self._refreshStatus)
-        self._refreshTimer.start(1000)
+        self._refreshTimer.start(500)
 
         self.exitOnClose = False
 
@@ -52,35 +64,132 @@ class MainWindow(QtGui.QMainWindow):
     # ----------------------------------------------------------------------
     def setupMenu(self):
 
-        self._soundAction = QtGui.QAction("Turn on sound", self,
+        self._soundAction = QtGui.QAction("Turn off sound", self,
                                                triggered=self._trunOnSound)
 
-        self._serverAction = QtGui.QAction("Start server", self,
+        self._serverAction = QtGui.QAction("Stop server", self,
                                                triggered=self._startServer)
 
         self._closeAction = QtGui.QAction("Exit", self,
                                                triggered=self._quitMe)
 
+        self._testMessage = QtGui.QAction("Test Notify", self,
+                                               triggered=self._sendTest)
+
         menubar = self.menuBar()
         menubar.addAction(self._soundAction)
-        menubar.addAction(self._serverAction)
+        self._soundAction.setEnabled(False)
+        # menubar.addAction(self._serverAction)
+        # menubar.addAction(self._testMessage)
         menubar.addAction(self._closeAction)
 
     # ----------------------------------------------------------------------
+    def _sendTest(self):
+
+        self.notifyChannel.send('Test!')
+
+    # ----------------------------------------------------------------------
     def _refreshStatus(self):
-        frame = self.device_proxy.Frame
-        if self.device_proxy.Frame.max() < 15:
+
+        try:
+            maxLm4 = self.LM4_proxy.Frame.max()
+        except:
+            maxLm4 = False
+
+        try:
+            v2State = self.V2_proxy.stellung[0]
+        except:
+            v2State = False
+
+        try:
+            PCurrent = self.PETRA_proxy.BeamCurrent
+            gotPETRA = True
+        except:
+            gotPETRA = False
+
+        try:
+            ps2State = self.PS2_proxy.stellung[0]
+        except:
+            ps2State = False
+
+        beamOk = True
+
+        if v2State:
+            if int(v2State) != 2:
+                beamOk = False
+                self._ui.lbV2.setText("closed")
+                self._ui.lbV2.setStyleSheet('color: red')
+            else:
+                self._ui.lbV2.setText("open")
+                self._ui.lbV2.setStyleSheet('color: green')
+        else:
+            self._ui.lbV2.setText("NC")
+            self._ui.lbV2.setStyleSheet('color: orange')
+
+        if maxLm4:
+            if maxLm4 < 15:
+                beamOk = False
+                self._ui.lbLM4.setText("no beam")
+                self._ui.lbLM4.setStyleSheet('color: red')
+            else:
+                self._ui.lbLM4.setText("OK")
+                self._ui.lbLM4.setStyleSheet('color: green')
+        else:
+            self._ui.lbLM4.setText("NC")
+            self._ui.lbLM4.setStyleSheet('color: orange')
+
+        if gotPETRA:
+            self._ui.lbPetra.setText('{}'.format(int(PCurrent)))
+            if int(PCurrent) < 80:
+                beamOk = False
+                self._ui.lbPetra.setStyleSheet('color: red')
+            elif int(PCurrent) < 98:
+                self._ui.lbPetra.setStyleSheet('color: orange')
+            else:
+                self._ui.lbPetra.setStyleSheet('color: green')
+        else:
+            self._ui.lbPetra.setText('NC')
+            self._ui.lbPetra.setStyleSheet('color: orange')
+
+        if ps2State:
+            if int(ps2State) != 2:
+                beamOk = False
+                self._ui.lbPS2.setText("closed")
+                self._ui.lbPS2.setStyleSheet('color: red')
+            else:
+                self._ui.lbPS2.setText("open")
+                self._ui.lbPS2.setStyleSheet('color: green')
+        else:
+            self._ui.lbPS2.setText("NC")
+            self._ui.lbPS2.setStyleSheet('color: orange')
+
+        if not beamOk:
             self.status = False
-            self._ui.lbStatus.setText("OFF!!!")
+            self._ui.lbStatus.setText("not OK")
             self._ui.lbStatus.setStyleSheet('color: red')
 
             if self.playSound:
+                self._soundAction.setText('Turn off sound')
+                self._soundAction.setEnabled(True)
                 playsound.playsound('./{}.mp3'.format(self.sound), True)
 
+            if not self.lostNotificationSent:
+                self.notifyChannel.send('Beam lost')
+                self.backNotificationSent = False
+                self.lostNotificationSent = True
         else:
+            self.playSound = True
+            self._soundAction.setEnabled(False)
             self.status = True
-            self._ui.lbStatus.setText("ON")
+            self._ui.lbStatus.setText("OK")
             self._ui.lbStatus.setStyleSheet('color: green')
+
+            if not self.backNotificationSent:
+                self.notifyChannel.send('Beam back')
+                self.backNotificationSent = True
+                self.lostNotificationSent = False
+
+        self.statusBarLabel.setText("Sound: {}".format(str(self.playSound)))
 
     # ----------------------------------------------------------------------
     def _trunOnSound(self):
@@ -93,14 +202,14 @@ class MainWindow(QtGui.QMainWindow):
     # ----------------------------------------------------------------------
     def _startServer(self):
         if not self._thread_running:
-            self._worker.start()
+            self.run()
         else:
             self.stopServerBucket.put('1')
 
     # ----------------------------------------------------------------------
     def closeEvent(self, event):
         if self.exitOnClose:
-            self.stopServerBucket.put('1')
+            # self.stopServerBucket.put('1')
             event.accept()
         else:
             event.setAccepted(True)
@@ -109,7 +218,7 @@ class MainWindow(QtGui.QMainWindow):
     # ----------------------------------------------------------------------
     def _showMe(self):
         self.show()
-    # ----------------------------------------------------------------------
+    #----------------------------------------------------------------------
     def _quitMe(self):
         self.exitOnClose = True
         self.close()
@@ -120,7 +229,7 @@ class MainWindow(QtGui.QMainWindow):
     def run(self):
         """
         """
-        print ("VISA Gate server on port {}".format(self.port))
+        print ("VISA Gate server on port {}".format(self._serverPort))
 
         self._socket.listen(1)
         self._thread_running = True
